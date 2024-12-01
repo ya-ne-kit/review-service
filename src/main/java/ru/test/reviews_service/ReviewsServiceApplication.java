f88ba6ab33822f3f9109d1ca170448f910b2c055
ya-ne-kit <120.23@mail.ru>
2024-12-01 15:01:17 +0300

    init

---
+++ src/main/java/ru/test/reviews_service/ReviewsServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.test.reviews_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsServiceApplication.class, args);
+	}
+
+}
